23c07f147097220f55dd9602e8b3f7a12ba03cb7
pbrudny <pbrudny@gmail.com>
2017-07-05 15:18:02 +0200

    Add program

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,6 +11,8 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+  end
 
+  def faq
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'static_pages/schedule'
   get 'static_pages/details'
   get 'static_pages/contact'
+  get 'static_pages/faq'
 
   resources :users
   root 'static_pages#main'
