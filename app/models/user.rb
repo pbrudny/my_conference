82a8e97c1bdbbd360b2e38a00668e28a4d0bad4e
pbrudny <pbrudny@gmail.com>
2017-07-11 10:32:10 +0200

    Adjust mailers

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   end
 
   def self.donors_not_registered
-    Donate.select{ |d| d.users.count == 0 }
+    Donate.select { |d| d.users.count == 0 }
   end
 
   def self.locked_seats
