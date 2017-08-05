a893883e56e8f9b83b6d0c48b94e72129fe3ca97
pbrudny <pbrudny@gmail.com>
2017-08-05 12:50:03 +0200

    Fix question order

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,30 +3,23 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def qa
-
+    @questions = Question.order(:id)
   end
-  # GET /questions
-  # GET /questions.json
+
   def index
     @questions = Question.order(:id)
   end
 
-  # GET /questions/1
-  # GET /questions/1.json
   def show
   end
 
-  # GET /questions/new
   def new
     @question = Question.new
   end
 
-  # GET /questions/1/edit
   def edit
   end
 
-  # POST /questions
-  # POST /questions.json
   def create
     @question = Question.new(question_params)
 
@@ -41,8 +34,6 @@ class QuestionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /questions/1
-  # PATCH/PUT /questions/1.json
   def update
     respond_to do |format|
       if @question.update(question_params)
@@ -55,8 +46,6 @@ class QuestionsController < ApplicationController
     end
   end
 
-  # DELETE /questions/1
-  # DELETE /questions/1.json
   def destroy
     @question.destroy
     respond_to do |format|
