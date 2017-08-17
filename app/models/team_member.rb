7a6bf6e019a26766512e49e9f769d9ef845b09d2
pbrudny <pbrudny@gmail.com>
2017-08-17 22:32:53 +0200

    fix csv

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -20,4 +20,13 @@ class TeamMember < ApplicationRecord
   def self.rooms_left
     self.accommodation_left / 2
   end
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |result|
+        csv << result.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
