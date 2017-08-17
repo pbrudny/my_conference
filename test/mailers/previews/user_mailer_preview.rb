c1ec2f23f30ee21011df23c0155b0cd1fad00d01
pbrudny <pbrudny@gmail.com>
2017-08-17 11:16:47 +0200

    Add companions to mail

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,7 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/created
   def user_registered
-    UserMailer.user_registered(User.first)
+    UserMailer.user_registered(User.last)
   end
 
   def user_accepted
